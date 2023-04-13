c66628615c8f3fb953e86a902b4b898ffdb3946f
enson_chen <1004714132@qq.com>
2023-04-13 16:58:56 +0800

    feat: ✨ css 属性转化成逻辑属性

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,52 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+const postcssRtlLogicalProperties = require('postcss-rtl-logical-properties');
+const postcss = require('postcss');
 
+/**
+ * 思路很简单就是在获取当对应的文本后，对对应的 CSS 值进行转化逻辑 CSS 即可。
+ * @param context 
+ */
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 // 插件激活的入口
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "transform-css-logic" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('transform-css-logic.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('transform-css-logic.transformFile', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from transform-css-logic!');
+		const editor = vscode.window.activeTextEditor; // 当前活动的编辑器
+		if (!editor) {
+			vscode.window.showInformationMessage('Unknown problem from transform-css-logic!');
+			return;
+		}
+		const doc = editor.document;
+		const text = doc.getText();
+
+		const result = postcss([
+			postcssRtlLogicalProperties(),
+		]).process(text);
+
+		const startPosition = new vscode.Position(0, 0);
+        const endPosition = new vscode.Position(doc.lineCount + 1, 0);
+		const range = new vscode.Range(startPosition, endPosition);
+		editor.edit(editBuilder => {
+			editBuilder.replace(range, result.css);
+		  }).then(success => {
+			// 返回是否应用修改
+			if (success) {
+			  console.log('Edit applied!');
+			} else {
+			  vscode.window.showInformationMessage('Try again!');
+			}
+		});
 	});
 
+
+	 // 给插件订阅 transform-css-logic.transformFile 命令
 	context.subscriptions.push(disposable);
 }
 
